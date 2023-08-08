abf5c59bf907671d5248bd3483adea7f03415b52
mmman <mangeshdborude@gmail.com>
2023-08-08 23:12:04 +0530

    Master is created by mangesh

---
+++ OrangeHRMApp/src/test/java/TestLayer/LoginPageTest.java
@@ -0,0 +1,26 @@
+package TestLayer;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
+
+public class LoginPageTest extends BaseClass {
+
+	private LoginPage loginpage;
+
+	@Parameters({ "browsername" })
+	@BeforeTest(groups = {"EndToEndTesting","LoginPage"})
+	public void setUp(String browsername) {
+		initialization(browsername);
+	}
+
+	@Test(groups = {"EndToEndTesting","LoginPage"})
+	public void validateLoginPageFunctionaliy() {
+		loginpage = new LoginPage();
+		loginpage.loginFunctionality("Admin", "admin123");
+	}
+
+}
